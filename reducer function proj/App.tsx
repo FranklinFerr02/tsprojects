2bce888e2deb68061ef70fe53af0ccfeeb44737a
FranklinFerr02 <112736788+FranklinFerr02@users.noreply.github.com>
2024-09-20 14:06:27 -0500

    Add files via upload

---
+++ reducer function proj/App.tsx
@@ -0,0 +1,12 @@
+import Component from './starter/07-reducers';
+
+function App() {
+  return (
+    <main>
+      <h2>react and typescript</h2>
+      <Component/>
+    </main>
+  );
+}
+
+export default App;
